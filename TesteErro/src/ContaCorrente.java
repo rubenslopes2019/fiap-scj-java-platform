2f3ae81bef7fd955874810049290750bdd0df6d3
Logon Aluno <logonaluno@local.com>
2019-10-31 21:55:12 -0300

    aula 3

--- TesteErro/src/ContaCorrente.java
+++ TesteErro/src/ContaCorrente.java
@@ -1,18 +1,21 @@
 
 public class ContaCorrente {
-	
+
 	private double saldo;
 
 	public void deposita(double valor) {
 		this.saldo += valor;
-	
+
 	}
 
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public void saca(double valor) throws SaldoInsuficiente {
+		if (this.saldo < valor) {
+			throw new SaldoInsuficiente("saldo insuficiente para saque");
+		}
 		this.saldo -= valor;
 	}
 
--- TesteErro/src/TesteErro.java
+++ TesteErro/src/TesteErro.java
@@ -1,10 +1,12 @@
+import javax.swing.JOptionPane;
+
 public class TesteErro {
 
 	public static void main(String[] args) {
 		System.out.println("inicio main");
 		try {
-			metodo1();		
-		} catch(NullPointerException e) {
+			metodo1();
+		} catch (NullPointerException e) {
 			System.out.println("teve um erro");
 		}
 
@@ -20,20 +22,37 @@ public class TesteErro {
 	private static void metodo2() {
 		System.out.println("inicio metodo2");
 		ContaCorrente cc = new ContaCorrente();
-		
-		
+		metodo3(cc);
+
 //		try {
-			for (int i = 0; i <= 15; i++) {
-				cc.deposita(i + 1000);
-				System.out.println(cc.getSaldo());
-				if (i == 5) {
-					cc = null;
-				}
-			}
+//			for (int i = 0; i <= 15; i++) {
+//				cc.deposita(i + 1000);
+//				System.out.println(cc.getSaldo());
+//				if (i == 5) {
+//					cc = null;
+//				}
+//			}
 //		} catch (NullPointerException e) {
 //			System.out.println("erro: " + e);
 //		}
 
 		System.out.println("fim do metodo2");
 	}
+
+	static void metodo3(ContaCorrente cc) {
+
+		Double valueDeposita = Double.valueOf(JOptionPane.showInputDialog("valor de depósito"));
+		cc.deposita(valueDeposita);
+		System.out.println("valor final: " + cc.getSaldo());
+
+		Double valueSacado = Double.valueOf(JOptionPane.showInputDialog("valor de saque"));
+		try {
+			cc.saca(valueSacado);
+			System.out.println("valor final: " + cc.getSaldo());
+		} catch (SaldoInsuficiente e) {
+			System.out.println("Saldo insuficiente");
+		}
+
+	}
+
 }
