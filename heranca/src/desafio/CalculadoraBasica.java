7ec8160b4b09ec1b51c4c0ce5a1ad13d21a27fc0
rubens <rlps.silva@gmail.com>
2019-11-18 01:13:38 -0200

    merge

---
+++ heranca/src/desafio/CalculadoraBasica.java
@@ -0,0 +1,17 @@
+package desafio;
+
+public interface class CalculadoraBasica {
+	
+/*	Crie uma classe que terá o comportamento de uma calculadora. Essa classe
+	deverá ter a interface CalculadoraBasica a seguir. Depois de desenvolver a
+	classe, teste o seu funcionamento, verificando todos os métodos da interface.
+	Ut i l i z e a s a í d a e m c o n s o l e p a r a i n f o rma r o r e s u l t a d o e o
+	JOptionPane.showInputDialog para a entrada de dados do usuário.
+*/
+	
+	public float somar(float op1, float op2);
+	public float subtrair(float op1, float op2);
+	public float dividir(float op1, float op2);
+	public float multiplicar(float op1, float op2);
+	
+}
