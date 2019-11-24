4c849e77bf2d1d50c12b0d8608658e62c0b50cf9
rubens <rlps.silva@gmail.com>
2019-11-24 20:04:14 -0200

    bot

--- rubenslssbot/src/rubenslssbot/Main.java
+++ rubenslssbot/src/rubenslssbot/Main.java
@@ -1,6 +1,7 @@
 package rubenslssbot;
 
 import java.text.Normalizer;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -20,6 +21,7 @@ import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -34,6 +36,20 @@ public class Main {
 		// controle de off-set, isto é, a partir deste ID será lido as mensagens
 		// pendentes na fila
 		int m = 0;
+		
+		
+		ContaCorrente cc = new ContaCorrente("Rubens", 1234, "0001");
+		ContaPoupanca cp = new ContaPoupanca("Rubens", 1234, "0001");
+		cc.deposita(1000);
+		
+		boolean exibeOpcao = false;
+		boolean perguntaSaldo = false;
+		boolean perguntaDeposito = false;
+		boolean perguntaSaque = false;
+		boolean perguntaTranferencia = false;
+		String operacao = "";
+		int opcaoEscolhida = 0;
+		
 		// loop infinito pode ser alterado por algum timer de intervalo curto
 		while (true) {
 			// executa comando no Telegram para obter as mensagens pendentes a partir de um
@@ -66,124 +82,81 @@ public class Main {
 				saudacao.add("noite");
 				saudacao.add("dia");
 				
+				Collections.sort(saudacao);
+				
 				List< String > despedida = new ArrayList<>();
 				despedida.add("tchau");
 				despedida.add("adeus");
 				despedida.add("fui");
 				despedida.add("obrigado");
-				despedida.add("logo");		
-				
-				List< String > ofensas = new ArrayList<>();
-				ofensas.add("burro");
-				ofensas.add("otario");
-				ofensas.add("babaca");
-				ofensas.add("idiota");
-				ofensas.add("viado");
-				ofensas.add("merda");
-				ofensas.add("animal");
-				ofensas.add("retardado");
-				ofensas.add("demente");
-				ofensas.add("jumento");
-				ofensas.add("bosta");
-				ofensas.add("merda");
-				ofensas.add("nada");
-				
-				Collections.sort(ofensas);
-				Collections.sort(saudacao);
+				despedida.add("logo");					
+
 				Collections.sort(despedida);
 				
-				
-				
-				Pizza atum = new Pizza();	
-				atum.setCodigo(1);
-				atum.setNome("atum");
-				atum.setPreco(40.0);
-				
+				List< String > pergunta = new ArrayList<>();
+				pergunta.add("quero");
+				pergunta.add("gostaria");
+				pergunta.add("preciso");
+				pergunta.add("operacao");
+				pergunta.add("conta");	
+				pergunta.add("dinheiro");
+				pergunta.add("corrente");
+				pergunta.add("poupanca");
+				pergunta.add("grana");
+				pergunta.add("opcao");
+				pergunta.add("menu");
 
-				Pizza bauru = new Pizza();
-				bauru.setCodigo(2);
-				bauru.setNome("bauru");
-				bauru.setPreco(34.0);		
+				Collections.sort(pergunta);
 				
-				Pizza calabresa = new Pizza();
-				calabresa.setCodigo(3);
-				calabresa.setNome("calabresa");
-				calabresa.setPreco(36.0);
-				
-				
-				Pizza baiana = new Pizza();
-				baiana.setCodigo(4);
-				baiana.setNome("baiana");
-				baiana.setPreco(38.0);
-				
-				Pizza frango = new Pizza();
-				frango.setCodigo(5);
-				frango.setNome("frango");
-				frango.setPreco(42.0);
-				
-				Pizza napolitana = new Pizza();
-				napolitana.setCodigo(5);
-				napolitana.setNome("napolitana");
-				napolitana.setPreco(42.0);
-				
-				Pizza palmito = new Pizza();
-				palmito.setCodigo(7);
-				palmito.setNome("palmito");
-				palmito.setPreco(44.0);
-				
-				List< Pizza > pizzas = new ArrayList<>();
-				pizzas.add(atum);
-				pizzas.add(bauru);
-				pizzas.add(baiana);
-				pizzas.add(calabresa);
-				pizzas.add(frango);
-				pizzas.add(napolitana);
-				pizzas.add(palmito);				
-				//Collections.sort(pizzas);
+				List< String > saldo = new ArrayList<>();
+				saldo.add("sald");
+				saldo.add("tot");
+				saldo.add("tenho");
+				saldo.add("cons");
+				saldo.add("ver");		
 
+				Collections.sort(saldo);
+				
+				List< String > deposito = new ArrayList<>();
+				deposito.add("depo");
+				deposito.add("coloc");
+				deposito.add("inse");
+		
 
-				Map<Integer,String> ingrediente = new HashMap<Integer,String>();				
-				ingrediente.put(1,"Molho de tomate fresco");
-				ingrediente.put(1,"atum especial sólido");
-				ingrediente.put(1,"cebola fatiada");
-				ingrediente.put(1,"orégano");
-				ingrediente.put(1,"azeitonas pretas");	
-				ingrediente.put(2,"Molho de tomate fresco");
-				ingrediente.put(2,"presunto cozido picado");
-				ingrediente.put(2,"mussarela especial"); 
-				ingrediente.put(2,"rodelas de tomate");
-				ingrediente.put(2,"orégano");
-				ingrediente.put(2,"azeitonas pretas");
-				ingrediente.put(3,"Molho de tomate fresco");
-				ingrediente.put(3,"calabresa especial fatiada");
-				ingrediente.put(3,"rodelas de cebola");
-				ingrediente.put(3,"orégano");
-				ingrediente.put(3,"azeitonas pretas");
-				ingrediente.put(4,"Molho de tomate fresco");
-				ingrediente.put(4,"calabresa moída levemente apimentada");
-				ingrediente.put(4,"ovos cozidos picados");
-				ingrediente.put(4,"cebola fatiada");
-				ingrediente.put(4,"orégano");
-				ingrediente.put(4,"azeitonas pretas");	
-				ingrediente.put(5,"Molho de tomate fresco");
-				ingrediente.put(5,"peito de frango desfiado levemente temperado");
-				ingrediente.put(5,"coberto com catupiry");
-				ingrediente.put(5,"orégano");
-				ingrediente.put(5,"azeitonas pretas");			
-				ingrediente.put(6,"Molho de tomate fresco");
-				ingrediente.put(6,"mussarela especial"); 
-				ingrediente.put(6,"rodelas de tomate");
-				ingrediente.put(6,"queijo parmesão");
-				ingrediente.put(6,"orégano");
-				ingrediente.put(6,"azeitonas pretas");
-				ingrediente.put(7,"Molho de tomate fresco");
-				ingrediente.put(7,"palmito macio em rodelas");
-				ingrediente.put(7,"coberto com catupiry");
-				ingrediente.put(7,"orégano");
-				ingrediente.put(7,"azeitonas pretas");
+				Collections.sort(deposito);
+				
+				List< String > saque = new ArrayList<>();
+				saque.add("sac");
+				saque.add("tir");
+				saque.add("peg");
+		
+				Collections.sort(saque);
+				
+				List< String > transferencia = new ArrayList<>();
+				transferencia.add("tran");
+				transferencia.add("jog");
+				transferencia.add("pass");
+
+				Collections.sort(transferencia);
+							
+				Map<Integer,String> opcao = new HashMap<Integer,String>();				
+				opcao.put(1,"consulta saldo");
+				opcao.put(2,"Depósito");
+				opcao.put(3,"Saque");
+				opcao.put(4,"Transferencia");
+				
+				Map<Integer,String> tipoConta = new HashMap<Integer,String>();				
+				tipoConta.put(1,"conta corrente");
+				tipoConta.put(2,"conta poupança");
+
 
 			
 				boolean achou = false;
+
+				int index;
+				Integer valorInt = 0;
+				double valorDouble = 0;
+				boolean ehNumero;
 
 
 				StringTokenizer palavras = new StringTokenizer(resposta, " ");
@@ -193,52 +166,360 @@ public class Main {
 					
 					System.out.println(palavra);
 					
-					int index = Collections.binarySearch(ofensas, palavra);
-					 
-					if (index >= 0) {
-					    palavra = ofensas.get(index);
-						mensagem = palavra + " é você, seu " + palavra;
-						break;
-					}
-					
+			 
+					index = 0;
 					index = Collections.binarySearch(saudacao, palavra);
 					 
 					if (index >= 0) {
-					    palavra = ofensas.get(index);
+					    palavra = saudacao.get(index);
 						mensagem = "olá em que posso te ajudar?";
 						break;
 					}
 					
+					index = 0;
 					index = Collections.binarySearch(despedida, palavra);
 					 
 					if (index >= 0) {
-					    palavra = ofensas.get(index);
+					    palavra = despedida.get(index);
 						mensagem = "Até mais, amigo";
 						break;
 					}
 					
-					if (palavra.contains("custa")) {
-						achou = true;
+					
+					
+					if (perguntaSaldo || perguntaDeposito || perguntaSaque || perguntaTranferencia) {
+						if (palavra.contains("sim") || palavra.contains("s")){
+							if (perguntaSaldo) {
+								opcaoEscolhida = 1;
+							} else {
+								if (perguntaDeposito) {
+									opcaoEscolhida = 2;
+								} else {
+									if (perguntaSaque) {
+										opcaoEscolhida = 3;
+									} else {
+										if (perguntaTranferencia) {
+											opcaoEscolhida = 4;
+										} else {
+											opcaoEscolhida = 0;
+										}
+									}
+								}
+							}					
+						} else {
+							palavra = "opcao";
+							
+						}
+						perguntaSaldo = false;
+						perguntaDeposito = false;
+						perguntaSaque = false;
+						perguntaTranferencia = false;
 					}
 					
-					if (achou) {
-						for (int i = 0; i < pizzas.size(); i++) {
-							if (pizzas.get(i).getNome().equals(palavra)) {
-								mensagem = "A pizza de " + palavra + " custa R$ " + pizzas.get(i).getPreco();
-							}
 
+					
+					if ((palavra.indexOf("corren") >= 0) || palavra.contains("cc") || palavra.contains("c/c")) {
+						if (opcaoEscolhida == 1) {
+							mensagem = "seu saldo é de: " + NumberFormat.getCurrencyInstance().format(cc.getSaldo());
+							opcaoEscolhida = 0;
+							operacao = "";
+							break;	
 						}
-					}else {
-							mensagem = "Infelizmente não temos a opção " + palavra;							
+						if (opcaoEscolhida == 2) {
+							mensagem = "quanto você gostaria de depositar?";
+							opcaoEscolhida = 0;
+							operacao = "pagaValorDepositoCC";
+							break;	
+						}
+						if (opcaoEscolhida == 3) {
+							mensagem = "quanto você gostaria de sacar?";
+							opcaoEscolhida = 0;
+							operacao = "pegaValorSaqueCC";
+							break;	
+						}
+						if (opcaoEscolhida == 4) {
+							mensagem = "quanto você gostaria de tranferir da c/c para c/p?";
+							opcaoEscolhida = 0;
+							operacao = "pegaValorTranferenciaCC";
+							break;	
+						}
 					}
+					
+					if ((palavra.indexOf("poupa") >= 0) || palavra.contains("cp") || palavra.contains("c/p")) {
+						if (opcaoEscolhida == 1) {
+							mensagem = "seu saldo é de: " + NumberFormat.getCurrencyInstance().format(cp.getSaldo());
+							opcaoEscolhida = 0;
+							operacao = "";
+							break;	
+						}
+						if (opcaoEscolhida == 2) {
+							mensagem = "quanto você gostaria de depositar?";
+							opcaoEscolhida = 0;
+							operacao = "pagaValorDepositoCP"; 
+							break;	
+						}
+						if (opcaoEscolhida == 3) {
+							mensagem = "quanto você gostaria de sacar?";
+							opcaoEscolhida = 0;
+							operacao = "pegaValorSaqueCP";
+							break;	
+						}
+						if (opcaoEscolhida == 4) {
+							mensagem = "quanto você gostaria de tranferir da c/p para a c/c?";
+							opcaoEscolhida = 0;
+							operacao = "pegaValorTranferenciaCP";
+							break;	
+						}
+					}
+					
+					//double valor = Double.parseDouble(palavra);
+					
+
+					
+					try {
+						valorInt = (Integer.parseInt(palavra));
+						ehNumero = true;
+					} catch (NumberFormatException e) {
+						ehNumero = false;
+					}
+
+					if (ehNumero) {
+						if (valorInt > 0 && valorInt < 5) {
+							opcaoEscolhida = valorInt;
+						}
+					}
+					
 				
+
+					
+					if (operacao != "") {
+						
+						try {
+							valorDouble = (Double.parseDouble(palavra));
+							ehNumero = true;
+						} catch (NumberFormatException e) {
+							ehNumero = false;
+						}
+
+						if (ehNumero) {
+							if (valorDouble > 0) {
+								switch (operacao) {
+								case "pagaValorDepositoCC":
+									try {
+										cc.deposita(valorDouble);
+										mensagem = "depositado o valor de : " + NumberFormat.getCurrencyInstance().format(valorDouble);
+									} catch (Exception e) {
+										mensagem = e.toString();
+									}
+									break;
+								case "pegaValorSaqueCC":
+									try {
+										cc.saca(valorDouble);
+										mensagem = "sacado o valor de : " + NumberFormat.getCurrencyInstance().format(valorDouble);
+									} catch (SaldoInsuficienteException e) {
+										mensagem = "seu saldo é insuficiente";
+									}	
+									
+									break;								
+								case "pegaValorTranferenciaCC":
+									try {
+										cc.transfere(valorDouble, cp);
+										mensagem = "tranferido valor de : " + NumberFormat.getCurrencyInstance().format(valorDouble);
+									} catch (Exception e) {
+										mensagem = e.toString();
+									}
+									break;	
+								case "pagaValorDepositoCP":
+									try {
+										cp.deposita(valorDouble);
+										mensagem = "depositado o valor de : " + NumberFormat.getCurrencyInstance().format(valorDouble);
+									} catch (Exception e) {
+										mensagem = e.toString();
+									}
+									break;
+								case "pegaValorSaqueCP":		
+									try {
+										cp.saca(valorDouble);
+										mensagem = "sacado o valor de : " + NumberFormat.getCurrencyInstance().format(valorDouble);
+									} catch (Exception e) {
+										mensagem = e.toString();
+									}										
+									break;								
+								case "pegaValorTranferenciaCP":
+									try {
+										cp.transfere(valorDouble, cc);
+										mensagem = "tranferido o valor de : " + NumberFormat.getCurrencyInstance().format(valorDouble);
+									} catch (Exception e) {
+										mensagem = e.toString();
+									}
+									break;
+								default:
+									mensagem = "valor inválido";
+									break;
+								}		
+								operacao = "";
+							} else {
+								mensagem = "valor inválido";
+								operacao = "";
+							} 
+						} else {
+							mensagem = "valor inválido";
+							operacao = "";
+						}
+						
+					}	           
+
+
+					if (opcaoEscolhida > 0 && opcaoEscolhida < 5) {
+						mensagem = "você gostaria de acessar conta corrente ou poupança";
+
+						break;						
+					} else {
+						
+					}
+					
+					
+					if (palavra.indexOf("sald") >= 0) {
+						palavra = "sald";
+					}
+					if (palavra.indexOf("tot") >= 0) {
+						palavra = "tot";
+					}
+					if (palavra.indexOf("cons") >= 0) {
+						palavra = "cons";
+					}
+					if (palavra.indexOf("depo") >= 0) {
+						palavra = "depo";
+					}
+					if (palavra.indexOf("coloc") >= 0) {
+						palavra = "coloc";
+					}
+					if (palavra.indexOf("inse") >= 0) {
+						palavra = "inse";
+					}
+					
+					if (palavra.indexOf("sac") >= 0) {
+						palavra = "sac";
+					}
+					if (palavra.indexOf("saq") >= 0) {
+						palavra = "sac";
+					}
+					if (palavra.indexOf("tir") >= 0) {
+						palavra = "tir";
+					}
+					if (palavra.indexOf("peg") >= 0) {
+						palavra = "peg";
+					}
+					if (palavra.indexOf("tran") >= 0) {
+						palavra = "tran";
+					}
+					if (palavra.indexOf("jog") >= 0) {
+						palavra = "jog";
+					}
+					if (palavra.indexOf("pass") >= 0) {
+						palavra = "pass";
+					}
+					
+					
+					index = 0;
+					index = Collections.binarySearch(saldo, palavra);
+					 
+					if (index >= 0) {
+					    palavra = saldo.get(index);
+						mensagem = "você gostaria de consultar seu saldo? sim ou não.";
+						perguntaSaldo = true;
+						exibeOpcao = false;
+						break;
+					}
+
+					index = 0;
+					index = Collections.binarySearch(saque, palavra);
+					
+					if (index >= 0) {
+					    palavra = saque.get(index);
+						mensagem = "você gostaria de realizar um saque? sim ou não.";
+						perguntaSaque = true;
+						exibeOpcao = false;
+						break;
+					}
+					
+					index = 0;
+					index = Collections.binarySearch(deposito, palavra);
+					
+					if (index >= 0) {
+					    palavra = deposito.get(index);
+						mensagem = "você gostaria de realizar um depósito? 'sim' ou 'não'.";
+						perguntaDeposito = true;
+						exibeOpcao = false;
+						break;
+					}
+					
+					index = 0;
+					index = Collections.binarySearch(transferencia, palavra);
+					
+					if (index >= 0) {
+					    palavra = transferencia.get(index);
+						mensagem = "você gostaria de realizar uma transferência? sim ou não.";
+						perguntaTranferencia = true;
+						exibeOpcao = false;
+						break;
+					}
+					
+					index = 0;
+					index = Collections.binarySearch(pergunta, palavra);
+					
+					if (index >= 0) {
+					    palavra = pergunta.get(index);
+						mensagem = "Nós temos as seguintes opções:";
+						exibeOpcao = true;
+					}							
+									
 				}
 	
 				
 				sendResponse = bot.execute(new SendMessage(update.message().chat().id(), mensagem));
 				// verificação de mensagem enviada com sucesso
 				System.out.println("Mensagem Enviada?" + sendResponse.isOk()); //confirma
+				
+				
+				//exibir menu
+				
+				if(exibeOpcao) {
+					exibeOpcao = false;
+					for (Integer key : opcao.keySet()) {
+						String value = opcao.get(key);
+						sendResponse = bot.execute(new SendMessage(update.message().chat().id(), (key + " para " + value)));
+					}
+				}				
+				
 			}
 		}}
 
 }
+
+
+
+
+//Map<String, String> example = new HashMap<String, String>();
+//
+//example.put("K1", new String("V1"));
+//example.put("K2", new String("V2"));
+//example.put("K3", new String("V3"));
+//example.put("K4", new String("V4"));
+//example.put("K5", new String("V5"));
+//
+///*
+// * O método "keySet()" retorna um Set com todas as chaves do
+// * nosso HashMap, e tendo o Set com todas as Chaves, 
+//  * podemos facilmente pegar
+// * os valores que desejamos
+// * */
+// 
+//for (String key : example.keySet()) {
+//        
+//       //Capturamos o valor a partir da chave
+//       String value = example.get(key);
+//       System.out.println(key + " = " + value);
+//}
+//
+//}
